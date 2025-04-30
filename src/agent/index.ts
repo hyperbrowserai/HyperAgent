09f999adaf3d7501dfd483acb48b15645cdb2cc8
tusharmctrl <tushar@missionctrl.dev>
2025-04-30 14:22:36 +0530

    Add support for running automation for mobile devices to check UI/UX

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -1,6 +1,6 @@
 import { BaseChatModel } from "@langchain/core/language_models/chat_models";
 import { ChatOpenAI } from "@langchain/openai";
-import { Browser, BrowserContext, Page } from "playwright";
+import { Browser, BrowserContext, devices, Page } from "playwright";
 import { v4 as uuidv4 } from "uuid";
 
 import {
@@ -40,6 +40,7 @@ export class HyperAgent<T extends BrowserProviders = "Local"> {
   private tokenLimit = 128000;
   private debug = false;
   private mcpClient: MCPClient | undefined;
+  private useMobile: boolean = false;
   private browserProvider: T extends "Hyperbrowser"
     ? HyperbrowserProvider
     : LocalBrowserProvider;
@@ -75,6 +76,7 @@ export class HyperAgent<T extends BrowserProviders = "Local"> {
     } else {
       this.llm = params.llm;
     }
+    this.useMobile = params?.useMobile ?? false;
     this.browserProviderType = (params.browserProvider ?? "Local") as T;
 
     this.browserProvider = (
@@ -99,10 +101,22 @@ export class HyperAgent<T extends BrowserProviders = "Local"> {
    */
   public async initBrowser(): Promise<Browser> {
     if (!this.browser) {
+      const iphone12 = devices["iPhone 12"];
+
       this.browser = await this.browserProvider.start();
-      this.context = await this.browser.newContext({
-        viewport: null,
-      });
+      this.context = await this.browser.newContext(
+        this.useMobile
+          ? {
+              userAgent: iphone12.userAgent,
+              viewport: {
+                width: iphone12.viewport.width,
+                height: iphone12.viewport.height + 100,
+              },
+            }
+          : {
+              viewport: null,
+            }
+      );
 
       // Inject script to track event listeners
       await this.context.addInitScript(() => {
