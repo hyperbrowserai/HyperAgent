fb498f841e1afcc5cb98f18e75dfc596d95f058a
tusharmctrl <tushar@missionctrl.dev>
2025-05-01 17:29:40 +0530

    fix: remove unwanted type

--- src/types/config.ts
+++ src/types/config.ts
@@ -68,6 +68,5 @@ export interface HyperAgentConfig<T extends BrowserProviders = "Local"> {
     NonNullable<ConstructorParameters<typeof HyperbrowserProvider>[0]>,
     "debug"
   >;
-  useMobile?: boolean;
   localConfig?: ConstructorParameters<typeof LocalBrowserProvider>[0];
 }
