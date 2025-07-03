7590d1b4ee5a96bf43cb3cc6fc6c31abc229dbf6
QRex-v0 <217984643+QRex-v0@users.noreply.github.com>
2025-07-03 15:03:52 -0700

    fix bugs

--- src/agent/messages/system-prompt.ts
+++ src/agent/messages/system-prompt.ts
@@ -1,4 +1,4 @@
-import { INPUT_FORMAT } from "./input-format";
+import { INPUT_FORMAT, INPUT_FORMAT_FIND_ELEMENT } from "./input-format";
 import { OUTPUT_FORMAT, OUTPUT_FORMAT_FIND_ELEMENT } from "./output-format";
 import { EXAMPLE_ACTIONS } from "./examples-actions";
 
@@ -12,16 +12,12 @@ const DATE_STRING = new Date().toLocaleString(undefined, {
 export const SYSTEM_PROMPT_FIND_ELEMENT = `
 You are a smart and sophisticated agent that is designed to automate web browser interactions.
 You try to accomplish goals in a quick and concise manner.
-Your goal is to accomplish the final goal.
-
-# World State
-The current Date is ${DATE_STRING}. The date format is MM/DD/YYYY.
 
 # Input Format
-${INPUT_FORMAT}
+${INPUT_FORMAT_FIND_ELEMENT}
 
 # Output Format
-${OUTPUT_FORMAT}
+${OUTPUT_FORMAT_FIND_ELEMENT}
 `;
 
 export const SYSTEM_PROMPT = `You are a smart and sophisticated agent that is designed to automate web browser interactions.
